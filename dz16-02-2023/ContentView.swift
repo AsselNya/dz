4fd3231b8a08de5774ccd7fb24247a3e6d9d59a3
2131 <231>
2024-02-09 21:54:11 +0600

    Initial Commit

---
+++ dz16-02-2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dz16-02-2023
+//
+//  Created by Асель Ашенова on 09.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dz16-02-2023/dz16_02_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  dz16_02_2023App.swift
+//  dz16-02-2023
+//
+//  Created by Асель Ашенова on 09.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct dz16_02_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
